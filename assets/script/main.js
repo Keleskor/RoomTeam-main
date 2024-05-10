7c4d6a646739215aa25bc97eb925abbe8011884c
Ilya Shaparenkov <166312901+Keleskor@users.noreply.github.com>
2024-05-10 22:50:12 +0500

    Add files via upload

--- assets/script/main.js
+++ assets/script/main.js
@@ -36,6 +36,9 @@ const app = createApp({
     isTabletWidth(){
       return this.currentWidth<=1024;
     },
+    isPcWidth(){
+      return this.currentWidth>=1025;
+    },
     isRendered(){
       return this.rendered;
     }
@@ -49,3 +52,68 @@ const app = createApp({
 })
 app.config.warnHandler = () => null;
 app.use(ElementPlus).mount('#app')
+
+
+/*****************************************hamburger menu START******************************************/
+
+let button_burger_menu = document.querySelector('.button__burger-menu');
+let buttonClose_burger_menu = document.querySelector('.main-header__item__close');
+let main_header_block = document.querySelector('.main-header__block');
+let main_header = document.querySelector('.main-header');
+let body = document.querySelector('body');
+let block = document.querySelector('.block');
+
+button_burger_menu.addEventListener("click",()=>{
+  program_menu_modal.classList.remove('introduction__block__active');
+  main_header_block.classList.add('main-header__block__acitve');
+  block.classList.add('block__active');
+  body.classList.add('overflow');
+  introduction.style.zIndex = '2006';
+})
+buttonClose_burger_menu.addEventListener("click",()=>{
+  main_header_block.classList.remove('main-header__block__acitve');
+  block.classList.remove('block__active');
+  body.classList.remove('overflow');
+})
+
+/*****************************************hamburger menu END******************************************/
+
+/*****************************************programm menu modal START****************************************/
+
+let button_program_menu = document.querySelector('.button__program-menu');
+let buttonClose_program_menu = document.querySelector('.introduction__block__close');
+let program_menu_modal = document.querySelector('.introduction__block');
+let introduction = document.querySelector('.introduction');
+
+button_program_menu.addEventListener("click",()=>{
+  program_menu_modal.classList.add('introduction__block__active');
+  block.classList.add('block__active');
+  introduction.style.zIndex = '2008';
+  window.scrollTo(0,0);
+  body.classList.add('overflow');
+})
+buttonClose_program_menu.addEventListener("click",()=>{
+  program_menu_modal.classList.remove('introduction__block__active');
+  block.classList.remove('block__active');
+  introduction.style.zIndex = '2006';
+  body.classList.remove('overflow');
+})
+
+const mediaQuery = window.matchMedia('(max-width: 767px)')
+function handleTabletChange(e) {
+  if (e.matches) {
+    var slider1 = new Swiper('.swiper', {
+      navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev'
+      },
+      pagination: {
+         el: '.swiper-pagination',
+         clickable: true,
+      },
+    });
+  }
+}
+mediaQuery.addListener(handleTabletChange)
+handleTabletChange(mediaQuery)
+/*****************************************programm menu modal END*****************************************/
